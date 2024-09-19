916305cb337342f1a4a255910187921a3c2e2fbe
Megan Ong <meganong@megans-mbp.lan>
2024-09-18 22:59:58 -0700

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"; 
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Hero from "./components/custom/Hero";
 import CreateTrip from "./create-trip/index.jsx";
